9a2e99812220989c20c4b6750f2dac5026a32ab4
Humberto Pires Marques <humberto.marques@aluno.uece.br>
2017-08-01 18:47:17 -0300

    Tudo funciona. Mais ou menos.

--- main.cpp
+++ main.cpp
@@ -1,218 +1,110 @@
-#include <opencv2/video/tracking.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+#include <bits/stdc++.h>
+#define bug(x) cout << #x << " = " << (x) << '\n'
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+#include <eigen3/Eigen/Dense>
 
-#include <iostream>
-#include <ctype.h>
 
 using namespace cv;
 using namespace std;
+using namespace Eigen;
 
-static void help()
+using tensor = vector<Mat>;
+using kernel = tensor;
+
+Rect2i cut_image(const Mat &frame)
 {
-    // print a welcome message, and the OpenCV version
-    cout << "\nThis is a demo of Lukas-Kanade optical flow lkdemo(),\n"
-            "Using OpenCV version " << CV_VERSION << endl;
-    cout << "\nIt uses camera by default, but you can provide a path to video as an argument.\n";
-    cout << "\nHot keys: \n"
-            "\tESC - quit the program\n"
-            "\tr - auto-initialize tracking\n"
-            "\tc - delete all the points\n"
-            "\tn - switch the \"night\" mode on/off\n"
-            "To add/remove a feature point click it\n" << endl;
+    Mat _frame;
+    frame.copyTo(_frame);
+    putText(_frame, "Minimum size", Point(10, 10), FONT_HERSHEY_COMPLEX_SMALL, 0.6, Scalar(0, 255, 0));
+    rectangle(_frame, Rect(20, 20, 21, 21), Scalar(255, 0, 0));
+    Rect2i roi = selectROI("Tracker", _frame, false, false);
+
+    roi.width = max(roi.width, 21), roi.height = max(roi.height, 21);
+
+    if(roi.width % 2 == 0) roi.width++;
+    if(roi.height % 2 == 0) roi.height++;
+
+    return roi;
+}
+tensor get_features(const Mat &frame, const Rect2i &roi)
+{
+    Mat img = frame(roi), greyImg, dx, dy, d2x, d2y;
+    tensor features;
+    split(img, features);
+    cvtColor(img, greyImg, CV_BGR2GRAY);
+    Sobel(greyImg, dx, CV_32FC1, 1, 0, 1);
+    Sobel(greyImg, dy, CV_32F, 0, 1, 1);
+    Sobel(greyImg, d2x, CV_32F, 2, 0, 1);
+    Sobel(greyImg, d2y, CV_32F, 0, 2, 1);
+
+    features.push_back(dx);
+    features.push_back(dy);
+    features.push_back(d2x);
+    features.push_back(d2y);
+    return features;
 }
 
-Point2f point;
-bool addRemovePt = false;
-
-static void onMouse( int event, int x, int y, int /*flags*/, void* /*param*/ )
+Mat get_cov(const tensor &roi_tensor)
 {
-    if( event == EVENT_LBUTTONDOWN )
-    {
-        point = Point2f((float)x, (float)y);
-        addRemovePt = true;
-    }
+
+    int m = roi_tensor[0].rows, n = roi_tensor[0].cols, sz = roi_tensor.size();
+    Mat cov = Mat::zeros(sz, sz, CV_64F), mean = Mat::zeros(1, sz, CV_64F), aux = Mat::zeros(1, sz, CV_64F);
+
+    for(int i = 0; i < roi_tensor.size(); ++i)
+        mean.at<double>(0, i) = sum(roi_tensor[i])[0] / (m * n);
+    bug(mean);
+    for(int i = 0;  i < m; ++i)
+        for(int j = 0; j < n; ++j)
+        {
+            for(int k = 0; k < sz; ++k)
+                aux.at<double>(0, k) = roi_tensor[k].at<double>(i, j);
+            aux = (aux - mean);
+            cov += (aux.t()) * aux;
+        }
+    bug(cov);
+    return cov;
 }
 
-
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
-    //ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
     VideoCapture cap;
-    TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
-    Size subPixWinSize(10,10), winSize(31,31);
-
-    const int MAX_COUNT = 200;
-    bool needToInit = false;
-    bool nightMode = false;
-    bool alwaysOn = false;
-    int time = 0;
-    cv::CommandLineParser parser(argc, argv, "{@input||}{help h||}");
-    string input = parser.get<string>("@input");
-    if (parser.has("help"))
-    {
-        help();
-        return 0;
-    }
-    if( input.empty() )
-        cap.open(0);
-    else if( input.size() == 1 && isdigit(input[0]) )
-        cap.open(input[0] - '0');
+    if(argc < 2)
+        cap = VideoCapture(0);
     else
-        cap.open(input);
+        cap = VideoCapture(argv[1]);
+    if(!cap.isOpened())
+        return -1;
 
-    if( !cap.isOpened() )
-    {
-        cout << "Could not initialize capturing...\n";
-        return 0;
-    }
-    cap.open("/home/marques/Workspace/kanade/outputfile.mp4");
-
-    namedWindow( "LK Demo", 1 );
-    setMouseCallback( "LK Demo", onMouse, 0 );
-
-    Mat gray, prevGray, image, frame;
-    vector<Point2f> points[2];
-    //vector<float> dists;
-    //double mean, deviation;
-    Scalar mean, deviation;
-    Mat dists;
-    double Th1, Th2, distAux;
-    //int fps = cap.get(CV_CAP_PROP_FPS) + .5;
+    Mat frame;
+    int x, y;
+    Rect2i roi;
+    /*if(argc < 2)
+        for(int i = 0; i < 20; ++i) cap >> frame;
+    cap >> frame;
+    */
+    tensor feat;
     for(;;)
     {
         cap >> frame;
-        if( frame.empty() )
+        if(waitKey(1) == 't')
         {
-            cap.open("/home/marques/Workspace/kanade/outputfile.mp4");
-            needToInit = true;
-            continue;
-            //break;
+            roi = selectROI("First", frame, false, false);
+            x = roi.x + roi.width / 2 + 1, y = roi.y + roi.height / 2 + 1;
+            feat = get_features(frame, roi);
+            for(auto i: feat) bug(i);
+
+            for(size_t i = 0; i < feat.size(); ++i)
+                imshow("Feature" + to_string(i), feat[i]);
+            rectangle(frame, roi, Scalar(0, 255, 0), 0.5);
+            circle(frame, Point2i(x, y), 1.5, Scalar(255, 0, 0), 2);
+            get_cov(feat);
         }
 
-        frame.copyTo(image);
-        cvtColor(image, gray, COLOR_BGR2GRAY);
 
-        if( nightMode )
-            image = Scalar::all(0);
-
-        if( needToInit )
-        {
-            goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3, 0, 0.04);
-            cornerSubPix(gray, points[1], subPixWinSize, Size(-1,-1), termcrit);
-            addRemovePt = false;
-        }
-        else if( !points[0].empty() )
-        {
-            vector<uchar> status;
-            vector<float> err;
-            if(prevGray.empty())
-                gray.copyTo(prevGray);
-            //line(image, Point(102, 100), Point(255, 300),Scalar(0, 0, 155));
-            //for(int i = 0; i < points[0].size(); ++i)
-            //{
-                //line(image, Point((int) points[0][i].x, (int)points[0][i].y), Point((int) points[1][i].x,(int) points[1][i].y), Scalar(0, 0, 255));
-                //cout << points[0][i].x << "  " << points[0][i].y << '\n';
-            //}
-            calcOpticalFlowPyrLK(prevGray, gray, points[0], points[1], status, err, winSize,
-                                 3, termcrit, 0, 0.001);
-            size_t i, k;
-            for( i = k = 0; i < points[1].size(); i++ )
-            {
-                if( addRemovePt )
-                {
-                    if( norm(point - points[1][i]) <= 5 )
-                    {
-                        addRemovePt = false;
-                        continue;
-                    }
-                }
-
-                if( !status[i] )
-                    continue;
-
-                points[1][k++] = points[1][i];
-                circle( image, points[1][i], 2, Scalar(0,255,0), -1, 8);
-                arrowedLine(image, Point((int)points[0][i].x, (int)points[0][i].y),
-                                   Point((int)points[1][i].x, (int)points[1][i].y), Scalar(255, 100, 255), 1.2, CV_AA, 0, 0.6);
-                dists.push_back(norm(points[0][i] - points[1][i]));
-
-            }
-            points[1].resize(k);
-
-            if(points[1].size() < MAX_COUNT / 2)
-            {
-                needToInit = true;
-                continue;
-            }
-
-            meanStdDev(dists, mean, deviation);
-            Th1 = mean[0] + deviation[0];
-            Th2 = mean[0] - deviation[1];
-            //waitKey(50);
-            for(i = 0; i < dists.size[0]; ++i)
-            {
-                distAux = dists.at<double>(i);
-                //cout << distAux << '\n';
-                //if(distAux < 0. or fabs(distAux) < 10) continue;
-                if ((distAux > Th1))
-                {
-                     circle( image, points[1][i], 2, Scalar(0,0,255), 3, 8);
-                     //arrowedLine(image, Point((int)points[0][i].x, (int)points[0][i].y),
-                                        //Point((int)points[1][i].x, (int)points[1][i].y), Scalar(255, 0, 0), 1.2, CV_AA, 0, 0.6);
-                }
-            }
-           // cout << dists.size[0] << '\n';
-
-            //cout << dists.at<double>(1) << ' ' << deviation[0] << ' ' << mean[0] << endl;
-            //cout << "dist size = " << dists.size() << endl;
-
-            dists.~Mat();
-        }
-
-        if( addRemovePt && points[1].size() < (size_t)MAX_COUNT )
-        {
-            vector<Point2f> tmp;
-            tmp.push_back(point);
-            cornerSubPix( gray, tmp, winSize, Size(-1,-1), termcrit);
-            points[1].push_back(tmp[0]);
-            addRemovePt = false;
-        }
-        putText(image, "Threshold 1 = " + to_string(Th1), Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
-        line(image, Point(10, 40), Point(10 + Th1 , 40), Scalar(0, 255, 0), 2);
-        putText(image, "Threshold 2 = " + to_string(Th2), Point(10, 70), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 255), 1);
-        line(image, Point(10, 90), Point(10 + Th2 , 90), Scalar(0, 255, 255), 2);
-
-        needToInit = false;
-        imshow("LK Demo", image);
-        time++;
-
-        char c = (char)waitKey(10);
-        if( c == 27 )
-            break;
-        if(alwaysOn and time % 2 == 0)
-            time = 0, c = 'r';
-        switch( c )
-        {
-        case 'r':
-            needToInit = true;
-            break;
-        case 'c':
-            points[0].clear();
-            points[1].clear();
-            break;
-        case 'n':
-            nightMode = !nightMode;
-            break;
-        case 'a':
-            alwaysOn = !alwaysOn;
-        }
-
-        std::swap(points[1], points[0]);
-        cv::swap(prevGray, gray);
-        //waitKey(100);
+        imshow("Tracking", frame);
+        if(waitKey(30) == 27) break;
     }
-
     return 0;
 }
